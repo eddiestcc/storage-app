5fe33d5fe2f23271417005ba4c38ec2bc45782b3
Eddie Molina <eddiemolina@Eddies-MBP.lan>
2024-09-15 17:15:33 -0400

    installed react router to create routes, removing ternary statements for routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {React, useState} from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Signin from './Components/Signin/Signin';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Nav from './Components/Nav/Nav';
@@ -8,58 +9,37 @@ import Search from './Components/Search/Search';
 import Retail from './Components/Retail/Retail';
 import './CSS/App.css'
 
-// function RouteComponent() {
-//   const routeChange = {
-//     route: "signin"
-//   }
-//   const [route, setRoute] = useState('signin');
-// }
-
-
 
 function App() {
 
-  const setRoute = 'signin';
-
-    // SIGN IN ROUTE 
-    return setRoute === 'signin' ? (
-      <div className="App">
-         <div className="wrapper">
-            <div className="sidebar">
-            <Nav />
+    return (
+      // <div className="App">
+      //    <div className="wrapper">
+      //       <div className="sidebar">
+      //       <Nav />
+      //       </div>
+      //       <div className="main">
+      //       <Signin  />
+      //       </div>
+      //   </div>
+      // </div>
+      <>
+        <div className='App'>
+          <div className='wrapper'>
+            <div className='sidebar'>
+                <Nav />
+              </div>
+              <div className='main'>
+                <Routes>
+                  <Route path="/" element={<Signin />}/>
+                  <Route path="/dashboard" element={<Dashboard />}/>
+                  <Route path="/rental" element={<Rental />}/>
+                </Routes>
+              </div>
             </div>
-            <div className="main">
-            <Signin  />
-            </div>
-        </div>
-      </div>
-    )
-    // DASHBOARD ROUTE 
-    : setRoute === 'dashboard' ? (
-      <div className="App">
-         <div className="wrapper">
-            <div className="sidebar">
-            <Nav />
-            </div>
-            <div className="main">
-            <Dashboard  />
-            </div>
-        </div>
-      </div>
-    )
-    // RENTAL ROUTE 
-    : (
-      <div className="App">
-         <div className="wrapper">
-            <div className="sidebar">
-            <Nav />
-            </div>
-            <div className="main">
-            <Rental  />
-            </div>
-        </div>
-      </div>
-    )
+          </div>
+      </>
+      )
 }
 
 export default App;
